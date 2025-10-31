a9fb1b040aeed0fac3f44a7fd5d60b6f58eeb4ce
Teddie <177896454+0x6060@users.noreply.github.com>
2025-10-31 23:35:21 +0100

    Update validate.js

--- api/validate.js
+++ api/validate.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
 
   try {
     const { license } = req.body;
-    const validLicenses = [''];
+    const validLicenses = ['19_Cb1K_NJjlQCpWdbQQyl0bf3zX23GMFs58BzQqPygw'];
 
     const isValid = validLicenses.includes(license);
 
